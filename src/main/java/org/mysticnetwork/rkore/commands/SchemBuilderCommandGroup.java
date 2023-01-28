cf7966eab994a20e1692bd970f33047590047f7b
Reap_9 <84695502+Reap09@users.noreply.github.com>
2023-01-28 00:08:46 -0700

    more commands
    Signed-off-by: Reap_9 <84695502+Reap09@users.noreply.github.com>

--- src/main/java/org/mysticnetwork/rkore/commands/SchemBuilderCommandGroup.java
+++ src/main/java/org/mysticnetwork/rkore/commands/SchemBuilderCommandGroup.java
@@ -3,15 +3,22 @@ package org.mysticnetwork.rkore.commands;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.bukkit.Server;
+import org.bukkit.command.ConsoleCommandSender;
 import org.mysticnetwork.rkore.settings.Settings;
 import org.mineacademy.fo.annotation.AutoRegister;
 import org.mineacademy.fo.command.ReloadCommand;
 import org.mineacademy.fo.command.SimpleCommandGroup;
+import org.mysticnetwork.rkore.utils.ColorUtils;
+
+import static org.bukkit.Bukkit.getServer;
 
 
 @AutoRegister
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SchemBuilderCommandGroup extends SimpleCommandGroup {
+    Server server = getServer();
+    ConsoleCommandSender console = server.getConsoleSender();
 
     @Getter(value = AccessLevel.PRIVATE)
     private static final SchemBuilderCommandGroup instance = new SchemBuilderCommandGroup();
@@ -20,11 +27,22 @@ public final class SchemBuilderCommandGroup extends SimpleCommandGroup {
     protected void registerSubcommands() {
         if (Settings.FlySpeedLimiter.ENABLED) {
             registerSubcommand(new FlySpeedLimiterToggleBypass(this));
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aFly Speed Limiter Toggle Enabled"));
+        } else {
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cFly Speed Limiter Toggle Disabled"));
         }
+
+
         if (Settings.ClearLag.ENABLED) {
             registerSubcommand(new ClearLag(this));
+        } else {
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cClear lag Command Disabled"));
         }
-        registerSubcommand(new SchemCommandGive(this));
+
+
+        if (Settings.SchemBuilder.ENABLED) {
+            registerSubcommand(new SchemCommandGive(this));
+        }
         registerSubcommand(new ReloadCommand());
     }
 
