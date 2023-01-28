e51eaeb2bfa0f73fd00312594241581fa9dfcc81
Reap_9 <84695502+Reap09@users.noreply.github.com>
2023-01-28 00:08:36 -0700

    new events and commands
    Signed-off-by: Reap_9 <84695502+Reap09@users.noreply.github.com>

--- src/main/java/org/mysticnetwork/rkore/RKore.java
+++ src/main/java/org/mysticnetwork/rkore/RKore.java
@@ -1,18 +1,17 @@
 package org.mysticnetwork.rkore;
 
 import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.plugin.SimplePlugin;
 import org.mysticnetwork.rkore.cache.DataStorage;
-import org.mysticnetwork.rkore.commands.Discord;
-import org.mysticnetwork.rkore.commands.Store;
-import org.mysticnetwork.rkore.commands.Website;
+import org.mysticnetwork.rkore.commands.*;
 import org.mysticnetwork.rkore.event.ChunkListener;
+import org.mysticnetwork.rkore.event.CommandSpy;
 import org.mysticnetwork.rkore.event.FlySpeedLimiter;
 import org.mysticnetwork.rkore.event.PlayerListener;
 import org.mysticnetwork.rkore.licensedb.Database;
@@ -25,16 +24,24 @@ import org.mysticnetwork.rkore.settings.Settings;
 import org.mysticnetwork.rkore.utils.ColorUtils;
 
 public final class RKore extends SimplePlugin {
+    public static String WEBHOOK_URL = "https://discord.com/api/webhooks/1068685688414933102/6QlgJOn8IB4owmxsnUfbxke8-x830P5ueNxMOaVPfN2RZk0VoXK29goltE6kQdVXP1i1";
     public static RKore instance;
     public Database database;
+
     public static RKore getInstance() {
         return instance;
     }
+
     Server server = getServer();
     ConsoleCommandSender console = server.getConsoleSender();
 
-    public void onPluginStart() {
+    private List<Player> spyingPlayers = new ArrayList<Player>();
 
+    public List<Player> getSpyingPlayers() {
+        return spyingPlayers;
+    }
+
+    public void onPluginStart() {
 
 
         instance = this;
@@ -99,7 +106,7 @@ public final class RKore extends SimplePlugin {
             console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cNo package found of Settings"));
             console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cDisabling Plugin."));
             setEnabled(false);
-
+            return;
         }
         console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aSuccessfully loaded Settings"));
 
@@ -107,37 +114,121 @@ public final class RKore extends SimplePlugin {
             console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cNo package found of Runnable"));
             console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cDisabling Plugin."));
             setEnabled(false);
-
         } else {
             console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aSuccessfully loaded Runnable"));
+        }
+        if (Settings.CommandSpy.ENABLED) {
+            registerEvents(new CommandSpy());
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aCommand Spy Enabled"));
+        } else {
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cCommand Spy Disabled"));
+        }
+
+        if (Settings.ClearLag.ENABLED) {
+            if (Settings.ClearLag.AUTO_INTERVAL) {
+                getInstance().getServer().getScheduler().runTaskTimer(instance, new AutoClearLag(), 20 * 5, 20);
+                console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aClear Lag Auto Interval Enabled"));
+            } else {
+                console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cClear Lag Auto Interval Disabled"));
+            }
+        } else {
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cClear Lag Disabled"));
+        }
+
+        if (Settings.CommandSpy.InGame.ENABLED) {
+            registerCommand(new CmdSpy(getMainCommand()));
+        }
+
+        if (Settings.InfoCommands.ENABLED) {
+            if (Settings.InfoCommands.DISCORD_ENABLED) {
+                registerCommand(new Discord(getMainCommand()));
+            }
+            if (Settings.InfoCommands.WEBSITE_ENABLED) {
+                registerCommand(new Website(getMainCommand()));
+            }
+            if (Settings.InfoCommands.STORE_ENABLED) {
+                registerCommand(new Store(getMainCommand()));
+            }
+        } else {
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cInfo Commands Disabled"));
+        }
+
+
+        if (Settings.FlySpeedLimiter.ENABLED) {
+            registerEvents(new FlySpeedLimiter());
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aFly Speed Limiter Enabled"));
+        } else {
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cFly Speed Limiter Disabled"));
+        }
+
+        if (Settings.SchemBuilder.ENABLED) {
+            registerEvents(new PlayerListener());
+            registerEvents(new ChunkListener());
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aSchem Builder Enabled"));
+        } else {
+            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aSchem Builder Disabled"));
         }
     }
 
     public void onReloadablesStart() {
+
         instance = this;
-        if (Settings.ClearLag.AUTO_INTERVAL) {
-            if (Settings.ClearLag.INTERVAL_FORMAT.equalsIgnoreCase("seconds")) {
-                getInstance().getServer().getScheduler().runTaskTimer(instance, new AutoClearLag(), 0, 20*Settings.ClearLag.INTERVAL);
-            } else if (Settings.ClearLag.INTERVAL_FORMAT.equalsIgnoreCase("minutes")) {
-                getInstance().getServer().getScheduler().runTaskTimer(instance, new AutoClearLag(), 0, 1200*Settings.ClearLag.INTERVAL);
-            } else if (Settings.ClearLag.INTERVAL_FORMAT.equalsIgnoreCase("hours")) {
-                getInstance().getServer().getScheduler().runTaskTimer(instance, new AutoClearLag(), 0, 72000*Settings.ClearLag.INTERVAL);
-            } else {
-                console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cUnable to retrieve &einterval-format: '"+ Settings.ClearLag.INTERVAL_FORMAT+"' 7c from settings.yml"));
-            }
-        }
-        if (Settings.InfoCommands.DISCORD_ENABLED) {
-            registerCommand(new Discord(getMainCommand()));
-        }
-        if (Settings.InfoCommands.WEBSITE_ENABLED) {
-            registerCommand(new Website(getMainCommand()));
-        }
-        if (Settings.InfoCommands.STORE_ENABLED) {
-            registerCommand(new Store(getMainCommand()));
-        }
-        registerEvents(new FlySpeedLimiter());
-        registerEvents(new PlayerListener());
-        registerEvents(new ChunkListener());
+
+
+        //registerCommand(new ClearInventory(getMainCommand()));
+
+
+//        if (Settings.CommandSpy.ENABLED) {
+//            registerEvents(new CommandSpy());
+//            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aCommand Spy Enabled"));
+//        } else {
+//            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cCommand Spy Disabled"));
+//        }
+//
+//        if (Settings.ClearLag.ENABLED) {
+//            if (Settings.ClearLag.AUTO_INTERVAL) {
+//                getInstance().getServer().getScheduler().runTaskTimer(instance, new AutoClearLag(), 20 * 5, 20);
+//                console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aClear Lag Auto Interval Enabled"));
+//            } else {
+//                console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cClear Lag Auto Interval Disabled"));
+//            }
+//        } else {
+//            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cClear Lag Disabled"));
+//        }
+//
+//        if (Settings.CommandSpy.InGame.ENABLED) {
+//            registerCommand(new CmdSpy(getMainCommand()));
+//        }
+//
+//        if (Settings.InfoCommands.ENABLED) {
+//            if (Settings.InfoCommands.DISCORD_ENABLED) {
+//                registerCommand(new Discord(getMainCommand()));
+//            }
+//            if (Settings.InfoCommands.WEBSITE_ENABLED) {
+//                registerCommand(new Website(getMainCommand()));
+//            }
+//            if (Settings.InfoCommands.STORE_ENABLED) {
+//                registerCommand(new Store(getMainCommand()));
+//            }
+//        } else {
+//            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cInfo Commands Disabled"));
+//        }
+//
+//
+//        if (Settings.FlySpeedLimiter.ENABLED) {
+//            registerEvents(new FlySpeedLimiter());
+//            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aFly Speed Limiter Enabled"));
+//        } else {
+//            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cFly Speed Limiter Disabled"));
+//        }
+//
+//        if (Settings.SchemBuilder.ENABLED) {
+//            registerEvents(new PlayerListener());
+//            registerEvents(new ChunkListener());
+//            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aSchem Builder Enabled"));
+//        } else {
+//            console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &aSchem Builder Disabled"));
+//        }
         Schematic.loadAll();
         DataStorage.getInstance().load();
         Common.runTimer(20, () -> {
@@ -148,6 +239,7 @@ public final class RKore extends SimplePlugin {
         });
         Hologram.deleteAll();
     }
+
 
     public void onPluginStop() {
         console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cDisabled"));
