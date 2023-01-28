e57b74d3733bba9ef0d3080aea870fe10dc48d07
Reap_9 <84695502+Reap09@users.noreply.github.com>
2023-01-28 00:06:38 -0700

    Added Discord Webhook with embed toggle with all the placeholders from CMDSPY with {player-head} as well
    Signed-off-by: Reap_9 <84695502+Reap09@users.noreply.github.com>

---
+++ src/main/java/org/mysticnetwork/rkore/commands/CmdSpy.java
@@ -0,0 +1,51 @@
+package org.mysticnetwork.rkore.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.mineacademy.fo.command.SimpleCommand;
+import org.mineacademy.fo.command.SimpleCommandGroup;
+import org.mysticnetwork.rkore.RKore;
+import org.mysticnetwork.rkore.settings.Settings;
+
+import java.util.List;
+
+public class CmdSpy extends SimpleCommand implements Listener {
+
+
+    public CmdSpy(SimpleCommandGroup parent) {
+        super("cmdspy");
+        setDescription(Settings.CommandSpy.InGame.DESCRIPTION);
+        setPermission(Settings.CommandSpy.InGame.PERMISSION);
+        setPermissionMessage(Settings.CommandSpy.InGame.NO_PERMISSION
+                .replace("{prefix}", Settings.PREFIX));
+        setAliases(Settings.CommandSpy.InGame.ALIASES);
+        setMinArguments(0);
+    }
+
+    RKore plugin = (RKore) Bukkit.getPluginManager().getPlugin("RKore");
+    List<Player> spyingPlayers = plugin.getSpyingPlayers();
+
+
+    protected void onCommand() {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (args.length == 0) {
+                if (spyingPlayers.contains(player)) {
+                    spyingPlayers.remove(player);
+                    player.sendMessage(Settings.CommandSpy.InGame.TOGGLE_MESSAGE
+                            .replace("{prefix}", Settings.PREFIX)
+                            .replace("{toggle}", Settings.CommandSpy.InGame.OFF_PLACEHOLDER));
+                } else {
+                    spyingPlayers.add(player);
+                    player.sendMessage(Settings.CommandSpy.InGame.TOGGLE_MESSAGE
+                            .replace("{prefix}", Settings.PREFIX)
+                            .replace("{toggle}", Settings.CommandSpy.InGame.ON_PLACEHOLDER));
+                }
+            }
+        } else {
+            sender.sendMessage(Settings.CommandSpy.InGame.ONLY_PLAYER
+                    .replace("{prefix}", Settings.PREFIX));
+        }
+    }
+}
---
+++ src/main/java/org/mysticnetwork/rkore/event/CommandSpy.java
@@ -0,0 +1,165 @@
+package org.mysticnetwork.rkore.event;
+
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.cacheddata.CachedDataManager;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.model.user.UserManager;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.mysticnetwork.rkore.RKore;
+import org.mysticnetwork.rkore.settings.Settings;
+import org.mysticnetwork.rkore.utils.ColorUtils;
+import org.mysticnetwork.rkore.utils.DiscordWebhook;
+import org.mysticnetwork.rkore.utils.PlayerHead;
+
+import java.awt.*;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+import static org.bukkit.Bukkit.*;
+
+public class CommandSpy implements Listener {
+
+    Server server = getServer();
+    ConsoleCommandSender console = server.getConsoleSender();
+    RKore plugin = (RKore) Bukkit.getPluginManager().getPlugin("RKore");
+    List<Player> spyingPlayers = plugin.getSpyingPlayers();
+
+    List<String> blackListedCmds = Settings.CommandSpy.InGame.BLACKLISTED_CMDS;
+
+    PlayerHead playerHead = new PlayerHead();
+    //ProxiedPlayer
+
+
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent e) {
+        Player player = e.getPlayer();
+        if (!player.hasPermission(Settings.CommandSpy.BYPASS_PERMISSION)) {
+            String cmd = e.getMessage();
+            String[] args = e.getMessage().split(" ");
+            if (this.blackListedCmds.contains(args[0]) || (args.length > 1 && this.blackListedCmds.contains(String.valueOf(args[0]) + " " + args[1])))
+                return;
+            if (!(Settings.CommandSpy.InGame.BLACKLISTED_CMDS.contains(blackListedCmds))) {
+                String executor = player.getName();
+                ZonedDateTime nowTime = ZonedDateTime.now(ZoneId.of(Settings.CommandSpy.TIMEZONE));
+                DateTimeFormatter timeFormatter = null;
+                try {
+                    timeFormatter = DateTimeFormatter.ofPattern(Settings.CommandSpy.TIMEZONE_FORMAT);
+                } catch (DateTimeParseException error) {
+                    console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] &cTime Format is incorrect!"));
+                }
+                assert timeFormatter != null;
+                String time = nowTime.format(timeFormatter);
+                long unixTime = nowTime.toEpochSecond();
+                String discordTime = "<t:" + unixTime + ">";
+                String discordRTime = "<t:" + unixTime + ":R>";
+                UserManager lp = LuckPermsProvider.get().getUserManager();
+                User user = lp.getUser(executor);
+                assert user != null;
+                String lpGroup = user.getPrimaryGroup();
+                CachedDataManager userData = user.getCachedData();
+                String lpPrefix = userData.getMetaData().getPrefix();
+                assert lpPrefix != null;
+                String translatedPrefix = ColorUtils.translateColorCodes(lpPrefix);
+                String serverName = player.getServer().getServerName();
+                String playerName = e.getPlayer().getName();
+
+                List<String> messages = Settings.CommandSpy.Discord.Embed.MESSAGES;
+                for (int i = 0; i < messages.size(); i++) {
+                    if (messages.get(i).isEmpty()) {
+                        messages.set(i, "\\n");
+                    }
+                }
+                String message = String.join("\\n", messages);
+                if (Settings.CommandSpy.InGame.ENABLED) {
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        if (spyingPlayers.contains(p)) {
+                            try {
+                                p.sendMessage(Settings.CommandSpy.InGame.MESSAGE
+                                        .replace("{player}", executor)
+                                        .replace("{cmd}", cmd)
+                                        .replace("{time}", time)
+                                        .replace("{lp-group}", lpGroup)
+                                        .replace("{lp-prefix}", translatedPrefix));
+
+                            } catch (NullPointerException error) {
+                                System.out.println(error);
+                            }
+                        }
+                    }
+                }
+                if (Settings.CommandSpy.Discord.ENABLED) {
+                    DiscordWebhook webhook = new DiscordWebhook(RKore.WEBHOOK_URL);
+                    webhook.setAvatarUrl(Settings.CommandSpy.Discord.WEBHOOK_AVATAR
+                            .replace("{player}", executor)
+                            .replace("{cmd}", cmd)
+                            .replace("{server}", serverName)
+                            .replace("{time}", discordTime)
+                            .replace("{r-time}", discordRTime)
+                            .replace("{lp-group}", lpGroup)
+                            .replace("{lp-prefix}", translatedPrefix));
+                    webhook.setUsername(Settings.CommandSpy.Discord.WEBHOOK_USERNAME
+                            .replace("{player}", executor)
+                            .replace("{cmd}", cmd)
+                            .replace("{server}", serverName)
+                            .replace("{time}", discordTime)
+                            .replace("{r-time}", discordRTime)
+                            .replace("{lp-group}", lpGroup)
+                            .replace("{lp-prefix}", translatedPrefix));
+                    if (!(Settings.CommandSpy.Discord.Embed.ENABLED)) {
+                        webhook.setContent(Settings.CommandSpy.Discord.MESSAGE
+                                .replace("{player}", executor)
+                                .replace("{cmd}", cmd)
+                                .replace("{server}", serverName)
+                                .replace("{time}", discordTime)
+                                .replace("{r-time}", discordRTime)
+                                .replace("{lp-group}", lpGroup)
+                                .replace("{lp-prefix}", translatedPrefix));
+                    } else {
+                        webhook.addEmbed(new DiscordWebhook.EmbedObject()
+                                .setColor(Color.decode(Settings.CommandSpy.Discord.Embed.COLOR))
+                                .setThumbnail(Settings.CommandSpy.Discord.Embed.THUMBNAIL.replace("{player-head}", playerHead.getPlayerHeadUrl(playerName)))
+                                .addField("", message
+                                        .replace("{player}", executor)
+                                        .replace("{cmd}", cmd)
+                                        .replace("{server}", serverName)
+                                        .replace("{time}", discordTime)
+                                        .replace("{r-time}", discordRTime)
+                                        .replace("{lp-group}", lpGroup)
+                                        .replace("{lp-prefix}", translatedPrefix), false)
+                                .setTitle(Settings.CommandSpy.Discord.Embed.TITLE
+                                        .replace("{player}", executor)
+                                        .replace("{cmd}", cmd)
+                                        .replace("{server}", serverName)
+                                        .replace("{time}", discordTime)
+                                        .replace("{r-time}", discordRTime)
+                                        .replace("{lp-group}", lpGroup)
+                                        .replace("{lp-prefix}", translatedPrefix))
+                                .setFooter(Settings.CommandSpy.Discord.Embed.FOOTER
+                                        .replace("{player}", executor)
+                                        .replace("{cmd}", cmd)
+                                        .replace("{time}", discordTime)
+                                        .replace("{r-time}", discordRTime)
+                                        .replace("{server}", serverName)
+                                        .replace("{lp-group}", lpGroup)
+                                        .replace("{lp-prefix}", translatedPrefix), ""));
+                    } try {
+                        webhook.execute();
+                    } catch (java.io.IOException ioException) {
+                        getLogger().severe(ioException.getStackTrace().toString());
+                        System.out.println(ioException);
+                    }
+                }
+            }
+        }
+    }
+}
