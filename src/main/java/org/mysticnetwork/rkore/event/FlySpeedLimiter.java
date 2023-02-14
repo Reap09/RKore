36fab70b0fb7e57a52705ab9fa6dcf4ccac91a52
Reap_9 <84695502+reap09@users.noreply.github.com>
2023-02-13 20:29:46 -0700

    fixed FlySpeedLimiter detection when enabling and disabling fly
    Signed-off-by: Reap_9 <84695502+Reap09@users.noreply.github.com>

--- src/main/java/org/mysticnetwork/rkore/event/FlySpeedLimiter.java
+++ src/main/java/org/mysticnetwork/rkore/event/FlySpeedLimiter.java
@@ -3,6 +3,7 @@ package org.mysticnetwork.rkore.event;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.util.Tristate;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Server;
@@ -11,6 +12,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.mysticnetwork.rkore.RKore;
 import org.mysticnetwork.rkore.settings.Settings;
 import org.mysticnetwork.rkore.utils.ColorUtils;
 
@@ -25,6 +28,7 @@ public class FlySpeedLimiter implements Listener {
     private final Double MAX_VERTICAL_FLY_SPEED = Settings.FlySpeedLimiter.MAX_VERTICAL_FLY_SPEED;
     private final String BYPASS_PERMISSION = Settings.FlySpeedLimiter.BYPASS_PERMISSION;
     private final String BYPASS_TOGGLE_PERMISSION = Settings.FlySpeedLimiter.BYPASS_TOGGLE_PERMISSION;
+    private boolean justEnabledFly = false;
 
 
     @EventHandler
@@ -37,45 +41,54 @@ public class FlySpeedLimiter implements Listener {
         Tristate bypassTogglePermission = user.getCachedData().getPermissionData().checkPermission(BYPASS_TOGGLE_PERMISSION);
         if (player.isFlying()) {
             if (!bypassTogglePermission.asBoolean() || !bypassPermission.asBoolean()) {
-                double xDiff = e.getTo().getX() - e.getFrom().getX();
-                double zDiff = e.getTo().getZ() - e.getFrom().getZ();
-                double horizontalSpeed = Math.sqrt(xDiff * xDiff + zDiff * zDiff);
-                horizontalSpeed = horizontalSpeed * 20;
-                double yDiff = e.getTo().getY() - e.getFrom().getY();
-                double verticalSpeed = Math.abs(yDiff);
-                verticalSpeed = verticalSpeed * 20;
-                if (Settings.FlySpeedLimiter.DEBUG_VERTICAL) {
-                    console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] [&bVertical&r] " + verticalSpeed));
-                }
-                if (Settings.FlySpeedLimiter.DEBUG_HORIZONTAL) {
-                    console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] [&eHorizontal&r] " + horizontalSpeed));
-                }
-                Location newLocation = e.getFrom();
-                if (horizontalSpeed > MAX_HORIZONTAL_FLY_SPEED && !Settings.FlySpeedLimiter.DEBUG_HORIZONTAL) {
-                    if (Settings.FlySpeedLimiter.KICK_ENABLED) {
-                        player.kickPlayer(ChatColor.translateAlternateColorCodes('&', String.join("\n", Settings.FlySpeedLimiter.KICK_MESSAGE)));
-                    } else {
-                        if (Settings.FlySpeedLimiter.SETBACK) {
-                            newLocation.setX(e.getFrom().getX());
-                            newLocation.setZ(e.getFrom().getZ());
-                            player.teleport(newLocation);
-                            e.getPlayer().sendMessage(Settings.FlySpeedLimiter.NO_PERMISSION_MESSAGE.replace("{prefix}", Settings.PREFIX));
+                if (justEnabledFly) {
+                    double xDiff = e.getTo().getX() - e.getFrom().getX();
+                    double zDiff = e.getTo().getZ() - e.getFrom().getZ();
+                    double horizontalSpeed = Math.sqrt(xDiff * xDiff + zDiff * zDiff);
+                    horizontalSpeed = horizontalSpeed * 20;
+                    double yDiff = e.getTo().getY() - e.getFrom().getY();
+                    double verticalSpeed = Math.abs(yDiff);
+                    verticalSpeed = verticalSpeed * 20;
+                    if (Settings.FlySpeedLimiter.DEBUG_VERTICAL) {
+                        console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] [&bVertical&r] " + verticalSpeed));
+                    }
+                    if (Settings.FlySpeedLimiter.DEBUG_HORIZONTAL) {
+                        console.sendMessage(ColorUtils.translateColorCodes("[&5RKore&r] [&eHorizontal&r] " + horizontalSpeed));
+                    }
+                    Location newLocation = e.getFrom();
+                    if (horizontalSpeed > MAX_HORIZONTAL_FLY_SPEED && !Settings.FlySpeedLimiter.DEBUG_HORIZONTAL) {
+                        if (Settings.FlySpeedLimiter.KICK_ENABLED) {
+                            player.kickPlayer(ChatColor.translateAlternateColorCodes('&', String.join("\n", Settings.FlySpeedLimiter.KICK_MESSAGE)));
+                        } else {
+                            if (Settings.FlySpeedLimiter.SETBACK) {
+                                newLocation.setX(e.getFrom().getX());
+                                newLocation.setZ(e.getFrom().getZ());
+                                player.teleport(newLocation);
+                                e.getPlayer().sendMessage(Settings.FlySpeedLimiter.NO_PERMISSION_MESSAGE.replace("{prefix}", Settings.PREFIX));
+                            }
                         }
                     }
-                }
-                if (verticalSpeed > MAX_VERTICAL_FLY_SPEED && !Settings.FlySpeedLimiter.DEBUG_VERTICAL) {
-                    if (Settings.FlySpeedLimiter.KICK_ENABLED) {
-                        player.kickPlayer(ChatColor.translateAlternateColorCodes('&', String.join("\n", Settings.FlySpeedLimiter.KICK_MESSAGE)));
-                    } else {
-                        if (Settings.FlySpeedLimiter.SETBACK) {
-                            newLocation.setY(e.getFrom().getY());
-                            player.teleport(newLocation);
-                            e.getPlayer().sendMessage(Settings.FlySpeedLimiter.NO_PERMISSION_MESSAGE.replace("{prefix}", Settings.PREFIX));
+                    if (verticalSpeed > MAX_VERTICAL_FLY_SPEED && !Settings.FlySpeedLimiter.DEBUG_VERTICAL) {
+                        if (Settings.FlySpeedLimiter.KICK_ENABLED) {
+                            player.kickPlayer(ChatColor.translateAlternateColorCodes('&', String.join("\n", Settings.FlySpeedLimiter.KICK_MESSAGE)));
+                        } else {
+                            if (Settings.FlySpeedLimiter.SETBACK) {
+                                newLocation.setY(e.getFrom().getY());
+                                player.teleport(newLocation);
+                                e.getPlayer().sendMessage(Settings.FlySpeedLimiter.NO_PERMISSION_MESSAGE.replace("{prefix}", Settings.PREFIX));
+                            }
                         }
                     }
                 }
             }
         }
+    }
+
+    @EventHandler
+    public void onPlayerToggleFly(PlayerToggleFlightEvent e) {
+        Bukkit.getScheduler().runTaskLater(RKore.instance, () -> {
+            justEnabledFly = e.isFlying();
+        }, 3);
     }
 }
 
