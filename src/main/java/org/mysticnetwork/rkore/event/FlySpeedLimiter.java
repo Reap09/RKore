8822db366afa36b9fd5490431825eacc0b992ba4
Reap_9 <84695502+Reap09@users.noreply.github.com>
2023-03-18 04:08:28 -0700

    removed a debug message
    Signed-off-by: Reap_9 <84695502+Reap09@users.noreply.github.com>

--- src/main/java/org/mysticnetwork/rkore/event/FlySpeedLimiter.java
+++ src/main/java/org/mysticnetwork/rkore/event/FlySpeedLimiter.java
@@ -95,8 +95,6 @@ public class FlySpeedLimiter implements Listener {
                         }
                     }
                 }
-            } else {
-                System.out.println(player + "is in water");
             }
         }
     }
